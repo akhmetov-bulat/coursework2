fe0e0a35f787b6359d77b1556cb4c04458457e0a
Bulat-Akhmetov <akhmetov.bulat@gmail.com>
2021-11-05 01:23:59 -0500

    add posts/<postid> app.route

--- app.py
+++ app.py
@@ -20,6 +20,23 @@ def index():
     return render_template('index.html',bookmarks = bookmarks, data = data, comments = comments)
 
 
+@app.route('/posts/<int:postid>')
+def post(postid:int):
+    data = read_json('data/data.json')
+    all_comments = read_json('data/comments.json')
+    cur_comments = []
+    bookmarks = read_json('data/bookmarks.json')
+    marked = False
+    for post in data:
+        if postid == posts["pk"]:
+            cur_post = post
+    for comment in all_comments:
+        if postid == comment["post_id"]:
+            cur_comments.append(comment)
+    if postid in bookmarks:
+        marked = True
+    return render_template('post.html', post=cur_post, comments=cur_comments, marked=marked)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
