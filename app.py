d8d932bfbc5fcb703ccc47b09dbc8d4eb9c80aee
Bulat-Akhmetov <akhmetov.bulat@gmail.com>
2021-11-09 02:18:58 -0600

    all done ver. 2.0

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from utils import read_json, write_json
+from utils import read_json, write_json, get_all_posts,get_posts_by_keyword, get_bookmarks
 from flask import Flask, request, Response, render_template, redirect
 
 app = Flask(__name__)
@@ -6,85 +6,43 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = read_json('data/data.json')
-    comments = read_json('data/comments.json')
-    bookmarks = read_json('data/bookmarks.json')
-    return render_template('index.html',bookmarks = bookmarks, data = data, comments = comments)
+    posts, bookmarks_count = get_all_posts()
+    return Response(render_template('index.html', posts=posts, bookmarks_count=bookmarks_count), status=200)
 
 
 @app.route('/posts/<int:postid>')
 def post(postid:int):
-    data = read_json('data/data.json')
-    all_comments = read_json('data/comments.json')
-    comments = []
-    bookmarks = read_json('data/bookmarks.json')
-    marked = False
-    for index in range(len(data)):
-        if data[index]['pk'] == postid:
-            data[index]["views_count"] = data[index]["views_count"] + 1
-            post = data[index]
-            break
-    write_json('data/data.json', data)
-    for comment in all_comments:
-        if postid == comment["post_id"]:
-            comments.append(comment)
-    if postid in bookmarks:
-        marked = True
-    return render_template('post.html', post=post, comments=comments, marked=marked)
+    posts, bookmarks_count = get_posts_by_keyword('pk', postid)
+    print(posts)
+    return Response(render_template('post.html', post=posts[0]), status=200)
 
 
 @app.route('/search/', methods=['GET'])
 def search_get():
     search_request = request.args.get("s")
-    found_posts = []
-    search = False
-    comments = read_json('data/comments.json')
-    bookmarks = read_json('data/bookmarks.json')
+    search = True if search_request else False
     if search_request:
-        search = True
-        data = read_json('data/data.json')
-        for post in data:
-            if search_request.casefold() in post['content'].casefold():
-                found_posts.append(post)
-    return render_template('search.html', found_posts=found_posts[0:10],
-                           search=search, comments=comments, bookmarks=bookmarks)
+        posts, bookmarks_count = get_posts_by_keyword('content', search_request)
+        return Response(render_template('search.html', posts=posts), search = search)
+    return Response(render_template('search.html'), status = 400)
 
 
 @app.route('/users/<username>')
 def user_posts(username):
-    users_posts = []
-    comments = read_json('data/comments.json')
-    bookmarks = read_json('data/bookmarks.json')
-    data = read_json('data/data.json')
-    for post in data:
-        if post['poster_name'] == username:
-            users_posts.append(post)
-    return render_template('user-feed.html', users_posts=users_posts, comments=comments, bookmarks=bookmarks)
+    posts, bookmarks_count = get_posts_by_keyword('poster_name', username)
+    return Response(render_template('user-feed.html', posts=posts, bookmarks_count=bookmarks_count),status=200)
 
 
 @app.route('/tag/<tagname>')
 def tag_posts(tagname):
-    tagname = "#" + tagname
-    tags_posts = []
-    comments = read_json('data/comments.json')
-    bookmarks = read_json('data/bookmarks.json')
-    data = read_json('data/data.json')
-    for post in data:
-        if tagname in post['content']:
-            tags_posts.append(post)
-    return render_template('tag.html', tags_posts=tags_posts, comments=comments, bookmarks=bookmarks)
+    posts, bookmarks_count = get_posts_by_keyword('content', '#' + tagname)
+    return Response(render_template('tag.html', posts=posts), status=200)
 
 
 @app.route('/bookmarks')
 def bookmarks_posts():
-    bookmarked_posts = []
-    comments = read_json('data/comments.json')
-    bookmarks = read_json('data/bookmarks.json')
-    data = read_json('data/data.json')
-    for post in data:
-        if post['pk'] in bookmarks:
-            bookmarked_posts.append(post)
-    return render_template('bookmarks.html', bookmarked_posts=bookmarked_posts, comments=comments, bookmarks=bookmarks)
+    posts = get_bookmarks()
+    return Response(render_template('bookmarks.html', posts=posts), status=200)
 
 
 @app.route('/bookmarks/add/<int:postid>')
@@ -93,7 +51,6 @@ def add_bookmark(postid:int):
     if postid not in bookmarks:
         bookmarks.append(postid)
     write_json('data/bookmarks.json', bookmarks)
-    # print(request.environ.get('HTTP_REFERER', request.remote_addr))
     return redirect(request.environ.get('HTTP_REFERER', request.remote_addr), code=302)
 
 
@@ -103,7 +60,6 @@ def remove_bookmark(postid:int):
     if postid in bookmarks:
         bookmarks.remove(postid)
     write_json('data/bookmarks.json', bookmarks)
-    # print(request.environ.get('HTTP_REFERER', request.remote_addr))
     return redirect(request.environ.get('HTTP_REFERER', request.remote_addr), code=302)
 
 
--- utils.py
+++ utils.py
@@ -12,3 +12,79 @@ def read_json(filename):
 def write_json(filename,json_data):
     with open(filename, 'w', encoding='utf-8',) as f:
         json.dump(json_data, f, ensure_ascii=False, indent='\t')
+
+
+def increase_view_count(data, post, postid):
+    for index in range(len(data)):
+        if data[index]['pk'] == postid:
+            data[index]["views_count"] = data[index]["views_count"] + 1
+            cur_post = data[index]
+            break
+    write_json('data/data.json', data)
+
+
+def get_all_posts():
+    data = read_json('data/data.json')
+    bookmarks = read_json('data/bookmarks.json')
+    comments = read_json('data/comments.json')
+    posts = []
+    for post in data:
+        post['marked'] = post['pk'] in bookmarks
+        post['comments_count'] = 0
+        for comment in comments:
+            if comment['post_id'] == post['pk']:
+                post['comments_count'] += 1
+        post['hashtags'] = []
+        text_split = post['content'].split(" ")
+        for i in range(len(text_split) - 1,0,-1):
+            if text_split[i].startswith('#'):
+                post['hashtags'].append(text_split.pop(i))
+        post['content'] = ' '.join(text_split)
+        posts.append(post)
+    return posts, len(bookmarks)
+
+
+def get_posts_by_keyword(keyword, value):
+    data = read_json('data/data.json')
+    bookmarks = read_json('data/bookmarks.json')
+    comments = read_json('data/comments.json')
+    posts = []
+    for i in range(len(data), 0, -1):
+        if str(value).casefold() in str(data[i-1][keyword]).casefold():
+            if keyword == 'pk':
+                data[i-1]["views_count"] += 1
+            data[i-1]['marked'] = data[i-1]['pk'] in bookmarks
+            data[i-1]['comments_count'] = 0
+            for comment in comments:
+                if comment['post_id'] == data[i-1]['pk']:
+                    data[i-1]['comments_count'] += 1
+            data[i-1]['hashtags'] = []
+            text_split = data[i-1]['content'].split(" ")
+            for j in range(len(text_split),0,-1):
+                if text_split[j-1].startswith('#'):
+                    data[i-1]['hashtags'].append(text_split.pop(j-1))
+            data[i-1]['content'] = ' '.join(text_split)
+            posts.append(data[i-1])
+    return posts, len(bookmarks)
+
+
+def get_bookmarks():
+    data = read_json('data/data.json')
+    bookmarks = read_json('data/bookmarks.json')
+    comments = read_json('data/comments.json')
+    posts = []
+    for post in data:
+        if post['pk'] in bookmarks:
+            post['marked'] = post['pk'] in bookmarks
+            post['comments_count'] = 0
+            for comment in comments:
+                if comment['post_id'] == post['pk']:
+                    post['comments_count'] += 1
+            post['hashtags'] = []
+            text_split = post['content'].split(" ")
+            for i in range(len(text_split),0,-1):
+                if text_split[i - 1].startswith('#'):
+                    post['hashtags'].append(text_split.pop(i))
+            post['content'] = ' '.join(text_split)
+            posts.append(post)
+    return posts
