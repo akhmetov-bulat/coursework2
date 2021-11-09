3cc60de5484f4bb8791dac84e47f1d81e4bd49d5
Bulat-Akhmetov <akhmetov.bulat@gmail.com>
2021-11-08 19:44:05 -0600

    all done 2. used jinja2 logic in html code

--- app.py
+++ app.py
@@ -16,21 +16,21 @@ def index():
 def post(postid:int):
     data = read_json('data/data.json')
     all_comments = read_json('data/comments.json')
-    cur_comments = []
+    comments = []
     bookmarks = read_json('data/bookmarks.json')
     marked = False
     for index in range(len(data)):
         if data[index]['pk'] == postid:
             data[index]["views_count"] = data[index]["views_count"] + 1
-            cur_post = data[index]
+            post = data[index]
             break
     write_json('data/data.json', data)
     for comment in all_comments:
         if postid == comment["post_id"]:
-            cur_comments.append(comment)
+            comments.append(comment)
     if postid in bookmarks:
         marked = True
-    return render_template('post.html', post=cur_post, comments=cur_comments, marked=marked)
+    return render_template('post.html', post=post, comments=comments, marked=marked)
 
 
 @app.route('/search/', methods=['GET'])
